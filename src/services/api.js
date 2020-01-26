2e35cb830c1e8fa8f3b0ebdffbb040b0ad10af8c
itsmegabrielr <gabriel.ribeiro@ipremios.com.br>
2020-01-26 09:48:40 -0300

    Adding a oficial url for socket and api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://192.168.0.102:3001"
+    baseURL: "https://devmappingapi.herokuapp.com"
 })
 
 export default api
--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import socketio from 'socket.io-client'
 
-const socket = socketio("http://192.168.0.102:3001", {
+const socket = socketio("https://devmappingapi.herokuapp.com", {
     autoConnect: false
 })
 
